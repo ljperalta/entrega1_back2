5141518a33a0f7cc476508862e446314495eb27f
ljperalta <leoperalta.lp24@gmail.com>
2025-04-20 23:14:00 -0300

    segundo commit

--- src/bd/conexion.js
+++ src/bd/conexion.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const mongoose = require("mongoose") 
 
-mongoose.connect("mongodb+srv://leoperaltalp24:CpaD4lmKeF6WddJr@cluster0.4usbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.URI)
   .then(() => console.log("MongoDB Atlas conectado"))
   .catch((err) => console.error("Error de conexión:", err));
 
